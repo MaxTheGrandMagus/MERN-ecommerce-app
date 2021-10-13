0a453884274cb58db7a8ccc0b9d7f15bab0763bb
Max <veyronmclord@gmail.com>
2021-10-13 21:12:57 +0600

    added products page, styled - header, footer, homescreen, productscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>SHOP</h1>
-    </div>
+    <Router>
+      <Header />  
+
+        <main className="py-3 ">
+          <Container>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/product/:id" component={ProductScreen} />
+          </Container>
+        </main>
+
+      <Footer />
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import products from '../products';
+import Rating from '../components/Rating';
+
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
+
+
+const ProductScreen = (props) => {
+  const product = products.find(p => p._id === props.match.params.id)
+  console.log(product);
+
+  return (
+    <>
+      <Link to="/" className="btn btn-light my-3">Go Back</Link> 
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroupItem><h3>{product.name}</h3></ListGroupItem>
+            <ListGroupItem>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>Description: {product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col><strong>${product.price}</strong></Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of stock'}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem className="d-grid gap-2">
+                <Button disabled={product.countInStock === 0}>Add to Cart</Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default ProductScreen
