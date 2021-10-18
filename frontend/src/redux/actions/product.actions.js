8cb649d58b169900d17d232ea99fc8c08512766b
Max <veyronmclord@gmail.com>
2021-10-19 04:13:09 +0600

    created actions, constants, reducers for cart screen page, added localstorage for cartItems and also added to store.js

--- frontend/src/redux/actions/product.actions.js
+++ frontend/src/redux/actions/product.actions.js
@@ -9,7 +9,7 @@ import {
 } from '../constants/product.constants';
 
 
-export const listProducts = () => async(dispatch) => {
+export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ 
       type: PRODUCT_LIST_REQUEST 
@@ -31,7 +31,7 @@ export const listProducts = () => async(dispatch) => {
 }
 
 
-export const listProductDetails = (id) => async(dispatch) => {
+export const listProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ 
       type: PRODUCT_DETAILS_REQUEST 
