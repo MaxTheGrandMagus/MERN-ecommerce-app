89f4f8e7744330e597464198443ca8462312ef36
Max <veyronmclord@gmail.com>
2021-10-19 00:35:29 +0600

    connect redux, created store, product - action, constants, reducers, added redux hooks to homescreen page, connected it with redux and fetched data

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './bootstrap.min.css'
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import './index.css';
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+import './bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
