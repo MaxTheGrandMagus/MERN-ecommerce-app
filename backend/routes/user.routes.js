c32d1a2fa983cf101f2d4b022f6778b7a2ba3c9f
Max <veyronmclord@gmail.com>
2021-10-19 23:50:15 +0600

    created auth.middleware for adding protection to auth routes, implemented getting user profile by token confirming

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/user.controller.js";
+import { authUser, getUserProfile } from "../controllers/user.controller.js";
+import { protect } from '../middleware/auth.middleware.js';
 
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 
 export default router;
