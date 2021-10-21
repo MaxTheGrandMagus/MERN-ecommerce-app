4868482a90eb171a1fc2a9f0bae34ac70d42b93a
Max <veyronmclord@gmail.com>
2021-10-21 17:52:12 +0600

    added registerUser func for backend, added password encrypting for registerUser with salt, added route for registering in user routes, tested it with postman

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile } from "../controllers/user.controller.js";
+import { authUser, getUserProfile, registerUser } from "../controllers/user.controller.js";
 import { protect } from '../middleware/auth.middleware.js';
 
 
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
+router.route('/').post(registerUser)
 
 
 export default router;
