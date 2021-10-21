7fc844672e389646bdc2f92cd2999269b0dc9eb4
Max <veyronmclord@gmail.com>
2021-10-21 23:43:20 +0600

    backend: added updateUserProfile func to user.controlller, added to user.routes, tested it with postman

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile, registerUser } from "../controllers/user.controller.js";
+import { authUser, getUserProfile, registerUser, updateUserProfile } from "../controllers/user.controller.js";
 import { protect } from '../middleware/auth.middleware.js';
 
 
-router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
 router.route('/').post(registerUser)
+router.post('/login', authUser)
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
+
 
 
 export default router;
