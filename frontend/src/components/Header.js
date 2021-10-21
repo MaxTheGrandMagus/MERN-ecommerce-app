b77c4726f05c7883673ff0f39be24da6148a88b0
Max <veyronmclord@gmail.com>
2021-10-21 22:45:57 +0600

    added login/logout func to header component, created action for logging out

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { logout } from '../redux/actions/user.actions';
+
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 
 const Header = () => {
+  
+  const dispatch = useDispatch();
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -18,9 +30,18 @@ const Header = () => {
               <LinkContainer to="/cart">
                 <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                </NavDropdown>   
+              ) : <LinkContainer to="/login">
+                    <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
+                  </LinkContainer>
+              }
+              
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/redux/actions/user.actions.js
+++ frontend/src/redux/actions/user.actions.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { 
   USER_LOGIN_FAIL, 
   USER_LOGIN_REQUEST, 
-  USER_LOGIN_SUCCESS 
+  USER_LOGIN_SUCCESS, 
+  USER_LOGOUT,
 } from "../constants/user.constants"
 
 
@@ -35,4 +36,12 @@ export const login = (email, password) => async(dispatch) => {
           : error.message, 
     })
   }
+}
+
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({
+    type: USER_LOGOUT
+  })
 }
