bc9af597842276b59bebdf8f8a9dfd339c4b4170
Max <veyronmclord@gmail.com>
2021-10-22 19:37:02 +0600

    created paymentscreen, added actions, reducers, constants

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 import { Container } from 'react-bootstrap'
 
@@ -28,6 +29,7 @@ const App = () => {
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?" component={CartScreen} />
             <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
             <Route path="/" component={HomeScreen} exact />
           </Container>
         </main>
---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+
+import { savePaymentMethod } from '../redux/actions/cart.actions';
+
+import { Form, Button, FormGroup, FormLabel, Col, FormCheck } from 'react-bootstrap';
+
+
+const PaymentScreen = ({ history }) => {
+  const dispatch = useDispatch();
+
+  const cart = useSelector(state => state.cart);
+  const { shippingAddress } = cart;
+
+  if(!shippingAddress) {
+      history.push('/shipping')
+  }
+
+  const [paymentMethod, setPaymentMethod] = useState('PayPal');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    history.push('/placeorder');
+  }
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <FormGroup>
+          <FormLabel as='legend'>Select Method</FormLabel>
+          <Col>
+            <FormCheck 
+              type='radio' 
+              label='PayPal or Credit Card' 
+              id='PayPal' 
+              name='paymentMethod'
+              value='PayPal'
+              checked 
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></FormCheck>
+            {/* <FormCheck 
+              type='radio' 
+              label='Stripe' 
+              id='Stripe' 
+              name='paymentMethod'
+              value='Stripe'
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></FormCheck> */}
+          </Col>
+        </FormGroup>
+        <Button type='submit' variant='primary'>Continue</Button>
+      </Form>
+    </FormContainer>
+  )
+}
+
+
+export default PaymentScreen;
