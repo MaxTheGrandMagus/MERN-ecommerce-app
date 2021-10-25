7593d5dc5be762fd990117eb169fbf0573f812f8
Max <veyronmclord@gmail.com>
2021-10-25 19:15:47 +0600

    backend: created getOrderById func in order controller to get an protected order by id, added it to order routes

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems } from "../controllers/order.controller.js";
+import { addOrderItems, getOrderById } from "../controllers/order.controller.js";
 import { protect } from '../middleware/auth.middleware.js';
 
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById)
 
 
 export default router;
