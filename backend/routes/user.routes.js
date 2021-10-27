c660642d7fa48c2b05d76009f4f7b8a90f1b36d8
Max <veyronmclord@gmail.com>
2021-10-27 18:56:08 +0600

    backend: created getUsers controller for getting users as an admin, and added to auth.middleware a func that allows getting users only for users with isAdmin: true field

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile, registerUser, updateUserProfile } from "../controllers/user.controller.js";
-import { protect } from '../middleware/auth.middleware.js';
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from "../controllers/user.controller.js";
+import { protect, admin } from '../middleware/auth.middleware.js';
 
 
-router.route('/').post(registerUser)
+router.route('/')
+    .post(registerUser)
+    .get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile')
     .get(protect, getUserProfile)
