bb0d6e8a5220b58ed4f7dd387f282de08758bb39
Max <veyronmclord@gmail.com>
2021-10-28 05:44:28 +0600

    backend: added delete user by id for admin

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from "../controllers/user.controller.js";
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser } from "../controllers/user.controller.js";
 import { protect, admin } from '../middleware/auth.middleware.js';
 
 
@@ -11,6 +11,7 @@ router.post('/login', authUser)
 router.route('/profile')
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile)
+router.route('/:id').delete(protect, admin, deleteUser)
 
 
 export default router;
