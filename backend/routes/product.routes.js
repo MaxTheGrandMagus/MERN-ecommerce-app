a7bc95e1a76c0c211cc0aa4c584d985fcdbbf8bd
Max <veyronmclord@gmail.com>
2021-10-28 16:15:58 +0600

    backend: added create/update controllers for products, added to routes, tested with postman

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -4,13 +4,16 @@ import {
     getProducts, 
     getProductById,
     deleteProduct,
+    createProduct,
+    updateProduct
 } from "../controllers/product.controller.js";
 import { protect, admin } from '../middleware/auth.middleware.js';
 
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id')
     .get(getProductById)
     .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct)
 
 export default router;
