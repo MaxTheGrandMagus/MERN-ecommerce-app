8d1ebe3ba24bfcf430e316fa0ba86be45b0f161b
Max <veyronmclord@gmail.com>
2021-10-28 17:29:47 +0600

    frontend: added create product action/constants/reducer, implemented it on ProductListScreen

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -6,6 +6,7 @@ import {
   productListReducer, 
   productDetailsReducer,
   productDeleteReducer,
+  productCreateReducer,
 } from './reducers/product.reducers';
 import { cartReducer } from './reducers/cart.reducers';
 import { 
@@ -41,6 +42,7 @@ const reducer = combineReducers({
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
   orderListMy: orderListMyReducer,
+  productCreate: productCreateReducer,
 });
 
 
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 
-import { listProducts, deleteProduct } from '../redux/actions/product.actions';
+import { listProducts, deleteProduct, createProduct } from '../redux/actions/product.actions';
+import { PRODUCT_CREATE_RESET } from '../redux/constants/product.constants';
 
 import { Table, Button, Row, Col } from 'react-bootstrap'; 
 
@@ -22,17 +23,25 @@ const ProductListScreen = ({ history, match }) => {
   const productDelete = useSelector(state => state.productDelete);
   const { loading:loadingDelete, success:successDelete, error:errorDelete } = productDelete;
 
+  const productCreate = useSelector(state => state.productCreate);
+  const { loading:loadingCreate, success:successCreate, error:errorCreate, product:createdProduct } = productCreate;
+
   useEffect(() => {
-    if(userInfo && userInfo.isAdmin) {
-      dispatch(listProducts())
-    }
-    else {
+    dispatch({ type: PRODUCT_CREATE_RESET })
+
+    if(!userInfo.isAdmin) {
       history.push('/login')
     }
-  }, [dispatch, history, userInfo, successDelete])
 
-  const createProductHandler = () => {
+    if(successCreate) {
+      history.push(`/admin/product/${createdProduct._id}/edit`)
+    } else {
+      dispatch(listProducts())
+    }
+  }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
 
+  const createProductHandler = () => {  
+    dispatch(createProduct())
   }
 
   const deleteProductHandler = (id) => {
@@ -56,6 +65,8 @@ const ProductListScreen = ({ history, match }) => {
       </Row>
       { loadingDelete && <Loader /> }
       { errorDelete && <Message variant='danger'>{errorDelete}</Message> }
+      { loadingCreate && <Loader /> }
+      { errorCreate && <Message variant='danger'>{errorCreate}</Message> }
       { loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
         <Table striped bordered hover responsive className='table-sm'>
           <thead>
