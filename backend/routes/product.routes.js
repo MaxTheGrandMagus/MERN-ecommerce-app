77eaef4317a8c6eeb621c241482a450bbc788dec
Max <veyronmclord@gmail.com>
2021-10-28 15:29:52 +0600

    backend: created product delete controller, added to routes, tested with postman

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductById } from "../controllers/product.controller.js";
+import { 
+    getProducts, 
+    getProductById,
+    deleteProduct,
+} from "../controllers/product.controller.js";
+import { protect, admin } from '../middleware/auth.middleware.js';
 
 
 router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
-
+router.route('/:id')
+    .get(getProductById)
+    .delete(protect, admin, deleteProduct)
 
 export default router;
